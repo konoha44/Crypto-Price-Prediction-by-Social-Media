1256313f94ce3de710b89c63c35310a62902e950
Ivan Snegirev <ivan.snegirev33@gmail.com>
2023-01-19 18:38:32 +0200

    the first commit the first version initial

---
+++ docs/index.rst
@@ -0,0 +1,24 @@
+.. Crypto Price Prediction by Social Media documentation master file, created by
+   sphinx-quickstart.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Crypto Price Prediction by Social Media documentation!
+==============================================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+   getting-started
+   commands
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='"Crypto Price Prediction by Social Media" is a repository that uses social media data to predict crypto prices. It includes algorithms, models, and techniques for data extraction, cleaning, analysis, and prediction. The repository aims to provide a comprehensive resource for those interested in using social media for crypto price prediction.',
+    author='konoha44',
+    license='',
+)
