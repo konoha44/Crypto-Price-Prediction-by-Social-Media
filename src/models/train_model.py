87b28a1b58447b3471666a4451e23f3179900f5b
Ivan Snegirev <ivan.snegirev33@gmail.com>
2023-02-13 12:03:52 +0200

    fix some noqaS

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -42,7 +42,7 @@ def compute_split_indexes(dataset: pd.DataFrame) -> List[List[int]]:
     return list(ts_split.split(dataset))
 
 
-def train_test_split(
+def train_test_split_custom(
     dataset: pd.DataFrame,
     train_indexes,
     test_indexes,
@@ -108,7 +108,49 @@ def bin_tr_by_fscore(
     return fb_array.max(), bin_treshhold
 
 
-def train_pipeline(dataset: Optional[pd.DataFrame] = None, **args) -> pd.DataFrame:  # noqa: WPS210
+def train_pipeline_iteration(  # noqa: WPS210
+    prepared_dataset: pd.DataFrame,
+    train_indexes: int,
+    test_indexes: int,
+) -> Tuple[Any, Any, Any, Any, Any]:
+    """Train and get result by one crosval step.
+
+    Parameters
+    ----------
+    train_indexes : int
+        index of the last observation for training
+    test_indexes : int
+        index of observation for testing
+
+    Returns
+    -------
+    Tuple[Any, Any, Any, Any, Any]
+        prediction, bin threshold, verdict, true_value, fb max value for training set
+    """
+    x_train, x_test, y_train, y_test = train_test_split_custom(prepared_dataset, train_indexes, test_indexes)
+    x_train_vec, x_test_vec = process_data(x_train, x_test)
+
+    preds_train, preds_test = train(x_train_vec, y_train, x_test_vec)
+    fb_max, fb_bin_tr = bin_tr_by_fscore(preds_train, y_train)
+    save_object(float(fb_bin_tr), "bin_threshold")
+
+    return preds_test[0], fb_bin_tr, preds_test[0] >= fb_bin_tr, y_test.iloc[0], fb_max
+
+
+def train_pipeline_all_iterations(dataset: pd.DataFrame) -> pd.DataFrame:
+    """Train and get result by all crosval steps."""
+    train_iter_results = []
+    for train_indexes, test_indexes in tqdm(compute_split_indexes(dataset)):
+        train_iter_results.append(train_pipeline_iteration(dataset, train_indexes, test_indexes))
+
+    train_pred_results = pd.DataFrame(
+        train_iter_results,
+        columns=["pred", "bin_treshhold", "verdict", "target", "fb_max"],
+    )
+    return train_pred_results
+
+
+def train_pipeline(dataset: Optional[pd.DataFrame] = None, **args) -> pd.DataFrame:
     """Start general training pipeline.
 
     — Load data
@@ -130,21 +172,10 @@ def train_pipeline(dataset: Optional[pd.DataFrame] = None, **args) -> pd.DataFra
     if not dataset:
         dataset = load_data()
     dataset = preprocess_data(dataset)
-    pred_true = []
 
-    for train_indexes, test_indexes in tqdm(compute_split_indexes(dataset)):
-        x_train, x_test, y_train, y_test = train_test_split(dataset, train_indexes, test_indexes)
-        x_train_vec, x_test_vec = process_data(x_train, x_test)
-
-        preds_train, preds_test = train(x_train_vec, y_train, x_test_vec)
-        fb_max, fb_bin_tr = bin_tr_by_fscore(preds_train, y_train)
-        save_object(float(fb_bin_tr), "bin_threshold")
-
-        pred_true.append([preds_test[0], fb_bin_tr, preds_test[0] >= fb_bin_tr, y_test.iloc[0], fb_max])
-
-    pred_true_df = pd.DataFrame(pred_true, columns=["pred", "bin_treshhold", "verdict", "target", "fb_max"])
-    logger.info(pred_true_df)
-    return pred_true_df
+    train_pred_results = train_pipeline_all_iterations(dataset)
+    logger.info(train_pred_results)
+    return train_pred_results
 
 
 if __name__ == "__main__":
